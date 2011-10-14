f17639181401808963084f7724edf8e701771a0a
khaug <kenneth@ebi.acuk>
2011-10-14 15:42:48 +0100

    New class to retrieve Investigation and Assay information from ISAcreator

--- Metabolomics/src/main/java/org/isatools/plugins/metabolights/assignments/io/ConfigurationLoader.java
+++ Metabolomics/src/main/java/org/isatools/plugins/metabolights/assignments/io/ConfigurationLoader.java
@@ -3,7 +3,6 @@ package org.isatools.plugins.metabolights.assignments.io;
 import org.apache.xmlbeans.XmlException;
 import org.isatools.isacreator.configuration.io.ConfigXMLParser;
 import org.isatools.isacreator.spreadsheet.TableReferenceObject;
-import org.isatools.isatab.configurator.schema.IsaTabConfigFileType;
 import org.isatools.isatab.configurator.schema.IsaTabConfigurationType;
 import org.isatools.isatab.configurator.schema.IsatabConfigFileDocument;
 
@@ -19,21 +18,39 @@ import java.io.IOException;
  */
 public class ConfigurationLoader {
 
-    private static final String CONFIGURATION_FILE_LOCATION = "/metabolomics_configuration/configuration.xml";
+    private static final String MS_CONFIGURATION_FILE_LOCATION  = "/metabolomics_configuration/configuration_ms.xml";
+    private static final String NMR_CONFIGURATION_FILE_LOCATION = "/metabolomics_configuration/configuration_nmr.xml";
+    private static final String SAMPLE_PREFIX = "opt_sample_";
+
 
     public ConfigurationLoader() {
     }
 
+    //Defaults to MS
     public TableReferenceObject loadConfigurationXML() throws XmlException, IOException {
+        return loadConfigurationXML(MS_CONFIGURATION_FILE_LOCATION);
+    }
 
+    public TableReferenceObject loadNMRConfigurationXML() throws XmlException, IOException {
+        return loadConfigurationXML(NMR_CONFIGURATION_FILE_LOCATION);
+    }
+
+    private TableReferenceObject loadConfigurationXML(String configFile) throws XmlException, IOException {
+
+        //Load the current settings file
         IsatabConfigFileDocument configurationFile = IsatabConfigFileDocument.Factory.parse(
-                getClass().getResourceAsStream(CONFIGURATION_FILE_LOCATION));
+                getClass().getResourceAsStream(configFile)
+        );
 
         ConfigXMLParser parser = new ConfigXMLParser("");
 
+        //Add columns defined in the configuration file
         for (IsaTabConfigurationType doc : configurationFile.getIsatabConfigFile().getIsatabConfigurationArray()) {
             parser.processTable(doc);
         }
+
+        //Add Sample "bin" columns for all sample rows in the assay
+        //TODO
 
         if (parser.getTables().size() > 0) {
             return parser.getTables().get(0);
