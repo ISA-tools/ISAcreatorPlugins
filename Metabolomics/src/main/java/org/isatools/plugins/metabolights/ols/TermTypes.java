02d19d7431410c4e840ad53e790807e3ec78108f
khaug <kenneth@ebi.acuk>
2011-10-27 09:06:22 +0100

    Dynamically read sample columns from the assay, add these as opt_sample_NN columns in the metabolite assignment sheet

---
+++ Metabolomics/src/main/java/org/isatools/plugins/metabolights/ols/TermTypes.java
@@ -0,0 +1,17 @@
+package org.isatools.plugins.metabolights.ols;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kenneth
+ * Date: 26/10/2011
+ * Time: 12:10
+ */
+public class TermTypes {
+    public static final String FORMULA      = "FORMULA synonym";
+    public static final String SMILES       = "SMILES synonym";
+    public static final String NAME         = "preferred name";
+    public static final String REL_SYNONYM  = "related synonym";
+    public static final String SYNONYM      = "exact synonym";
+    public static final String XREF         = "xref_analog";
+
+}
