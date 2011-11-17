be1eb0d34bd15eef3a2c7e61af35ac96eb26d638
pconesa <p.conesa.mingo@gmail.com>
2011-11-17 10:09:22 +0000

    Add MetaboliteCellRenderer (almost Empty)

---
+++ Metabolomics/src/main/java/org/isatools/plugins/metabolights/assignments/ui/MetaboliteCellRenderer.java
@@ -0,0 +1,7 @@
+package org.isatools.plugins.metabolights.assignments.ui;
+
+import org.isatools.isacreator.spreadsheet.SpreadsheetCellRenderer;
+
+public class MetaboliteCellRenderer extends SpreadsheetCellRenderer{
+
+}
