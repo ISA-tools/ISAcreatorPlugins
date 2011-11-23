277ca1abd66720aa00574f597c5be5fa666c7bef
pconesa <p.conesa.mingo@gmail.com>
2011-11-23 10:53:42 +0000

    Properties in upper case RemoteInfo class to retrieve remote info

---
+++ Metabolomics/src/main/java/org/isatools/plugins/metabolights/assignments/model/RemoteInfo.java
@@ -0,0 +1,45 @@
+package org.isatools.plugins.metabolights.assignments.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Properties;
+ 
+public class RemoteInfo {
+	
+	static Properties props = getRemoteInfo();
+	static enum remoteProperties{
+		VERSION, DOWNLOADURL;
+		public String toString(){
+			return name().toString();
+		}
+	}
+	static Properties getRemoteInfo(){
+		String reqUrl = "https://raw.github.com/EBI-Metabolights/ISAcreatorPlugins/master/Metabolomics/remoteInfo.properties";
+        try {
+        	
+        	Properties props = new Properties();
+            URL url = new URL(reqUrl.toString());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+            
+            props.load(url.openStream());
+            
+            return props;
+        } catch (IOException e) {
+            e.printStackTrace();
+           
+        }
+        return null;
+
+	}
+	static public String getProperty(remoteProperties propertyName){
+		
+		if (props == null) return null;
+		
+		return  props.getProperty(propertyName.toString());
+		
+	}
+	
+	
+}
